bf45bab314d5311567be134cf11011d32ea57d8b
LinChen <lchen@ideabinder.com>
2015-09-19 18:44:50 +0800

    add compatibilities

--- draggable.js
+++ draggable.js
@@ -1,170 +1,343 @@
 /*
- *author: Lin Chen
- *date: 2015-9-19
- *todo: make it configurable ...
+ * draggable.js
+ * author: Lin Chen
+ * todo: make it configurable ...
  */
 
-function Drag(selector, options){
-var box = document.querySelector(selector);
-var els = Array.prototype.slice.call(box.children);
-var isStart = false;
-var distX = distY = 0;
-var dragEl, onEl;
-var orgList, posArr;
+function Draggable(container, options) {
 
-init();
+  "use strict";
 
-function init(){
- initPos();
- eventBind();
+  if (!container) return;
+
+  var lowVersionIE = navigator.userAgent.match(/MSIE 8|MSIE 7/);
+  var box = container.length ? container[0] : container;
+  var childNodelist = box.children || (function(element) {
+    var clone = element.cloneNode(true);
+    var child = clone.childNodes;
+
+    for (var i = 0; i < child.length; i++) {
+      if (!(child[i] instanceof Element)) clone.removeChild(child[i]);
+    }
+
+    return child;
+  })(box);
+
+  var toArray = !lowVersionIE ? Array.prototype.slice : function() {
+    var ArrayLike = this;
+    var result = [];
+
+    if (!ArrayLike.length) return result;
+
+    for (var i = 0; i < ArrayLike.length; i++) {
+      result.push(ArrayLike[i]);
+    }
+
+    return result;
+  };
+
+  // Array.prototype.indexOf polyfill, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf#Polyfill
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(searchElement, fromIndex) {
+      var k;
+
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+
+      if (len === 0) {
+        return -1;
+      }
+
+      var n = +fromIndex || 0;
+
+      if (Math.abs(n) === Infinity) {
+        n = 0;
+      }
+
+      if (n >= len) {
+        return -1;
+      }
+
+      k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      while (k < len) {
+        if (k in O && O[k] === searchElement) {
+          return k;
+        }
+        k++;
+      }
+      return -1;
+    };
+  }
+
+  var elements = toArray.call(childNodelist);
+  var isStart = false;
+  var boxLeft = box.offsetLeft;
+  var boxTop = box.offsetTop;
+  var distanceX, distanceY, dragingElement, fromElement, cloneElement, beforeList, beforePositions;
+
+  init();
+
+  function init() {
+
+    setPositions();
+
+    eventBind();
+
+  }
+
+  function setPositions() {
+    box.style.position = "relative";
+
+    for (var i = 0; i < elements.length; i++) {
+      var temp = elements[i];
+
+      temp.style.left = temp.offsetLeft + "px";
+      temp.style.top = temp.offsetTop + "px";
+
+      setTimeout((function(el) {
+        return function() {
+          el.style.position = "absolute";
+        };
+      })(temp), 0);
+
+    }
+  }
+
+  function eventBind() {
+    addListener(box, 'mousedown', onMouseDown);
+  }
+
+  function onMouseDown(ev) {
+    var ev = ev || window.event;
+
+    var target = ev.target || ev.srcElement;
+    var evX = 0,
+      evY = 0;
+
+    if (lowVersionIE) {
+
+      if (ev.button !== 1) return;
+
+      ev.returnValue = false;
+
+      evX = ev.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      evY = ev.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+
+    } else {
+
+      if (ev.button !== 0) return;
+
+      ev.preventDefault();
+
+      evX = ev.pageX;
+      evY = ev.pageY;
+    }
+
+    fromElement = dragingElement = getDragingElement(target);
+    dragingElement.style.transition = "none";
+
+    distanceX = evX - (boxLeft + dragingElement.offsetLeft);
+    distanceY = evY - (boxTop + dragingElement.offsetTop);
+    beforePositions = getPositions(elements);
+    beforeList = getStaticList(elements);
+
+    addListener(document, 'mousemove', onMouseMove);
+    addListener(document, 'mouseup', onMouseUp);
+  }
+
+  function onMouseMove(ev) {
+    var ev = ev || window.event;
+    var evX = 0,
+      evY = 0;
+
+    if (lowVersionIE) {
+      ev.returnValue = false;
+
+      evX = ev.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      evY = ev.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+
+    } else {
+
+      ev.preventDefault();
+
+      evX = ev.pageX;
+      evY = ev.pageY;
+    }
+
+    var toElement = getElementByPosition(evX, evY, 0);
+
+    if (!isStart) {
+      isStart = true;
+
+      cloneElement = dragingElement.cloneNode(true);
+      cloneElement.style.left = boxLeft + dragingElement.offsetLeft + "px";
+      cloneElement.style.top = boxTop + dragingElement.offsetTop + "px";
+      dragingElement.style.visibility = "hidden";
+      document.body.appendChild(cloneElement);
+
+    } else {
+
+      cloneElement.style.left = evX - distanceX + "px";
+      cloneElement.style.top = evY - distanceY + "px";
+
+      if (!toElement || toElement === fromElement) return;
+
+      rearrange(toElement);
+      fromElement = toElement;
+    }
+
+  }
+
+  function onMouseUp(ev) {
+    var ev = ev || window.event;
+
+    if (isStart) {
+      dragingElement.style.visibility = "";
+      document.body.removeChild(cloneElement);
+      isStart = false;
+    }
+
+    dragingElement.style.transition = "";
+    dragingElement = fromElement = cloneElement = null;
+
+    removeListener(document, 'mousemove', onMouseMove);
+    removeListener(document, 'mouseup', onMouseUp);
+  }
+
+  function getDragingElement(clickedElement) {
+
+    if (clickedElement.parentNode === box) {
+
+      return clickedElement;
+
+    } else {
+
+      return getDragingElement(clickedElement.parentNode);
+    }
+
+  }
+
+  function getElementByPosition(x, y, index) {
+
+    if (index === elements.length) return null;
+
+    var i = index;
+    var el = beforeList[i];
+    var pos = beforePositions[i];
+
+    var elLeft = boxLeft + pos.left;
+    var elRight = elLeft + el.offsetWidth;
+    var elTop = boxTop + pos.top;
+    var elBottom = elTop + el.offsetHeight;
+
+    if (x > elLeft && x < elRight && y > elTop && y < elBottom) {
+      return beforeList[i];
+
+    } else {
+
+      return getElementByPosition(x, y, i + 1);
+    }
+
+  }
+
+  function getIndex(element) {
+    return beforeList.indexOf(element);
+  }
+
+  function rearrange(toElement) {
+    var fromIndex = getIndex(fromElement);
+    var toIndex = getIndex(toElement);
+
+    // console.log("from: ", fromIndex, "to: ", toIndex);
+
+    if (fromIndex < toIndex) {
+
+      if (lowVersionIE && toIndex === beforeList.length - 1) {
+        box.appendChild(elements[fromIndex]);
+      } else {
+        box.insertBefore(elements[fromIndex], elements[toIndex + 1]);
+      }
+
+      elements = toArray.call(childNodelist);
+
+      for (var i = fromIndex; i < toIndex; i++) {
+        elements[i].style.left = beforePositions[i].left + "px";
+        elements[i].style.top = beforePositions[i].top + "px";
+      }
+
+    } else {
+
+      box.insertBefore(elements[fromIndex], elements[toIndex]);
+
+      elements = toArray.call(childNodelist);
+
+      for (var i = fromIndex; i > toIndex; i--) {
+        elements[i].style.left = beforePositions[i].left + "px";
+        elements[i].style.top = beforePositions[i].top + "px";
+      }
+
+    }
+
+    dragingElement.style.left = beforePositions[i].left + "px";
+    dragingElement.style.top = beforePositions[i].top + "px";
+
+  }
+
+  function getPositions(elementArray) {
+    var result = [];
+
+    for (var i = 0; i < elementArray.length; i++) {
+      result.push({
+        left: elementArray[i].offsetLeft,
+        top: elementArray[i].offsetTop
+      });
+    }
+
+    return result;
+  }
+
+  function getStaticList(elementArray) {
+    return toArray.call(elementArray);
+  }
+
+  // utilities
+  function addListener(element, type, handler) {
+
+    if (element.addEventListener) {
+      element.addEventListener(type, handler, false);
+    } else if (element.attachEvent) {
+      element.attachEvent('on' + type, handler);
+    } else {
+      element['on' + type] = handler;
+    }
+
+  }
+
+  function removeListener(element, type, handler) {
+
+    if (element.removeEventListener) {
+      element.removeEventListener(type, handler);
+    } else if (element.detachEvent) {
+      element.detachEvent('on' + type, handler);
+    } else {
+      elemnet['on' + type] = null;
+    }
+
+  }
+
 }
 
-function initPos(){
- box.style.position = "relative";
- for(var i = 0; i < els.length; i++){
-   var curEl = els[i];
-   curEl.style.left = curEl.offsetLeft + "px";
-   curEl.style.top = curEl.offsetTop + "px";
-
-   setTimeout((function(el){
-     return function(){
-       el.style.position = "absolute";
-     };
-   })(curEl), 0);
- }
-}
-
-function eventBind(){
- box.addEventListener("mousedown", onMouseDown, false);
-}
-
-function onMouseDown(ev){
- ev.preventDefault();
-
- onEl = dragEl = getDragEl(ev.target);
- dragEl.style.transition = "none";
- distX = ev.pageX - (box.offsetLeft + dragEl.offsetLeft);
- distY = ev.pageY - (box.offsetTop + dragEl.offsetTop);
- posArr = getPosArr(els);
- orgList = getOrgList(els);
-
- document.addEventListener("mousemove", onMouseMove, false);
- document.addEventListener("mouseup", onMouseUp, false);
-}
-
-function onMouseMove(ev){
- var toEl = getElByPos(ev.pageX, ev.pageY);
-
- ev.preventDefault();
-
- if(!isStart){
-   isStart = true;
-   cloneEl = dragEl.cloneNode(true);
-   cloneEl.style.left = box.offsetLeft + dragEl.offsetLeft + "px";
-   cloneEl.style.top = box.offsetTop + dragEl.offsetTop + "px";
-   dragEl.style.visibility = "hidden";
-   document.body.appendChild(cloneEl);
- }else{
-   cloneEl.style.left = ev.pageX - distX + "px";
-   cloneEl.style.top = ev.pageY - distY + "px";
-
-   if(!toEl || toEl === onEl) return;
-   rePosEls(toEl);
-   onEl = toEl;
- }
-}
-
-function onMouseUp(ev){
- console.log(isStart);
- if(isStart){
-   dragEl.style.visibility = "";
-   document.body.removeChild(cloneEl);
-   isStart = false;
- }
-
- dragEl.style.transition = "";
- dragEl = cloneEl = null;
- document.removeEventListener("mousemove", onMouseMove);
- document.removeEventListener("mouseup", onMouseUp);
-}
-
-function getDragEl(el){
- if(el.parentNode === box){
-   return el;
- }else{
-   return getDragEl(el.parentNode);
- }
-}
-
-function getElByPos(x, y, index){
-
- if(index === els.length) return null;
-
- var i = index || 0;
- var el = orgList[i];
- var pos = posArr[i];
-
- var elLeft = box.offsetLeft + pos.left;
- var elRight = elLeft + el.offsetWidth;
- var elTop = box.offsetTop + pos.top;
- var elBottom = elTop + el.offsetHeight;
-
- if(x > elLeft && x < elRight && y > elTop && y < elBottom){
-   return orgList[i];
- }else{
-   return getElByPos(x, y, i+1);
- }
-
-}
-
-function getIndex(el){
- return orgList.indexOf(el);
-}
-
-function rePosEls(el){
- var prevIndex = getIndex(onEl);
- var toIndex = getIndex(el);
- var isForward = prevIndex < toIndex;
-
- // console.log("frome: ", prevIndex, "to: ", toIndex);
-
- if(isForward){
-   box.insertBefore(els[prevIndex], els[toIndex+1]);
-   els = Array.prototype.slice.call(box.children);
-
-   for(var i = prevIndex; i < toIndex; i++){
-     els[i].style.left = posArr[i].left + "px";
-     els[i].style.top = posArr[i].top + "px";
-   }
- }else{
-   box.insertBefore(els[prevIndex], els[toIndex]);
-   els = Array.prototype.slice.call(box.children);
-
-   for(var i = prevIndex; i > toIndex; i--){
-     els[i].style.left = posArr[i].left + "px";
-     els[i].style.top = posArr[i].top + "px";
-   }
- }
-
- dragEl.style.left = posArr[i].left + "px";
- dragEl.style.top = posArr[i].top + "px";
-
-}
-
-function getPosArr(elArr){
- var res = [];
- for(var i = 0; i < elArr.length; i++){
-   res.push({
-     left: elArr[i].offsetLeft,
-     top: elArr[i].offsetTop
-   });
- }
-
- return res;
-}
-
-function getOrgList(elArr){
- return Array.prototype.slice.call(elArr);
-}
-
+if(window.jQuery){
+  (function($){
+    $.fn.Draggable = function(options){
+      return this.each(function(index, el) {
+       Draggable(el[0], options); 
+      });
+    }
+  })(window.jQuery)
 }
